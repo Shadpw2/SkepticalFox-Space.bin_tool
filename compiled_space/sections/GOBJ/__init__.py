c735107c64a967555021818c38290e0f680f81f6
Simi4 <maxim.like.python@ya.ru>
2022-05-17 23:02:44 +0300

    Dump GOBJ to json instead of bin

--- compiled_space/sections/GOBJ/__init__.py
+++ compiled_space/sections/GOBJ/__init__.py
@@ -1,6 +1,6 @@
 """ GOBJ (?) """
 
-from _base_binary_section import *
+from _base_json_section import *
 
 
 
@@ -8,14 +8,23 @@ __all__ = ('GOBJ_Section_1_12_1',)
 
 
 
-class GOBJ_Section_1_12_1(Base_Binary_Section):
+class GOBJ_Section_1_12_1(Base_JSON_Section):
 	header = 'GOBJ'
 	int1 = 2
 
+	@row_seek(True)
 	def from_bin_stream(self, stream, row):
-		stream.seek(row.position)
-		self._data = stream.read(row.length)
-		self._exist = True
+		''' FIXME! '''
+		self._data = {}
+		self.read_vector(stream, '1', '<6I16f')
+		self.read_vector(stream, '2', '<20I')
+		rest_len = row.position + row.length - stream.tell()
+		self._data['3'] = stream.read(rest_len).hex()
 
 	def to_bin(self):
-		return self._data
+		''' FIXME! '''
+		res = b''
+		res += self.write_vector('1', '<6I16f')
+		res += self.write_vector('2', '<20I')
+		res += bytes.fromhex(self._data['3'])
+		return res
